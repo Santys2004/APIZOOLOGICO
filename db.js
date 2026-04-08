e85a3db46c21f7a37d8bfee51a7dc552a69d5d06
santiago <santiagosuarez727@gmail.com>
2026-04-08 13:24:56 -0500

    api completa antes de la clase

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+
+export const conectionDatabase = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB is connected");
+    }catch(error){
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
